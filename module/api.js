c38a8a0816d199974131667d783102a89f96578b
overwriter <9856mmm@gmail.com>
2024-06-28 05:55:14 +0800

    feat: api测试接口data传递字符串

--- module/api.js
+++ module/api.js
@@ -2,7 +2,8 @@ const createOption = require('../util/option.js')
 module.exports = (query, request) => {
   const method = query.method || 'POST'
   const uri = query.uri
-  const data = query.data || {}
+  const data =
+    typeof query.data === 'string' ? JSON.parse(query.data) : query.data || {}
   const crypto = query.crypto || ''
 
   let res = request(method, uri, data, createOption(query, crypto))

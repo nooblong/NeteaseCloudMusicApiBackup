07edbfed6d3844e7e5dcbd4526a2f7b40944ea2b
overwriter <9856mmm@gmail.com>
2024-12-22 22:25:49 +0800

    fix: 验证码检查可能不生效

--- module/login_qr_create.js
+++ module/login_qr_create.js
@@ -3,7 +3,7 @@ const QRCode = require('qrcode')
 const createOption = require('../util/option.js')
 module.exports = (query, request) => {
   return new Promise(async (resolve) => {
-    const url = `https://music.163.com/login?codekey=${query.key}`
+    const url = `http://music.163.com/login?codekey=${query.key}`
     return resolve({
       code: 200,
       status: 200,
